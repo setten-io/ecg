2bc14c5cc87f765f2a6f97235fd60aa9a5e559e3
etienne-napoleone <etienne.napoleone@gmail.com>
2022-07-19 18:39:12 +0200

    fix(error): allow large difference btwn variants

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,7 @@ use thiserror::Error;
 
 pub(crate) type Result<T> = std::result::Result<T, Error>;
 
+#[allow(clippy::large_enum_variant)]
 #[derive(Error, Debug)]
 pub(crate) enum Error {
     #[error("deserialization failed: {0}")]
