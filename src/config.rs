398309c1647e003440fa6b0a8c2cfc1bad36e316
etienne-napoleone <etienne.napoleone@gmail.com>
2022-07-29 02:15:19 +0200

    feat(config): add structure

---
+++ src/config.rs
@@ -0,0 +1,63 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub(crate) struct Config {
+    pub(crate) targets: HashMap<String, TargetConfig>,
+}
+
+#[derive(Deserialize)]
+pub(crate) struct TargetConfig {
+    pub(crate) url: String,
+    pub(crate) interval: u64,
+    pub(crate) clients: Vec<ClientsConfig>,
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "type", rename_all = "kebab-case")]
+pub(crate) enum ClientsConfig {
+    Lcd {
+        url: String,
+    },
+    SettenLcd {
+        project_id: String,
+        key: String,
+        network: String,
+        blockchain: String,
+    },
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use indoc::indoc;
+    use serde_yaml::from_str;
+
+    #[test]
+    fn deserialize_from_yaml_str() {
+        let yaml = indoc! {"
+            targets:
+              phoenix-validator:
+                url: https://betteruptime.com/something
+                interval: 30
+                clients:
+                  - type: lcd
+                    url: https://phoenix-lcd.terra.dev
+                  - type: setten-lcd
+                    project_id: ea08855653b64998bb47b2c03bf66de7
+                    key: 02215b36969446c28b22059e63b4301b
+                    network: phoenix
+                    blockchain: terra
+              kaiyo-validator:
+                url: https://betteruptime.com/something
+                interval: 30
+                clients:
+                  - type: lcd
+                    url: https://lcd.kaiyo.kujira.setten.io      
+        "};
+
+        from_str::<Config>(yaml).unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 use clap::Parser;
 
 mod cli;
+mod config;
 mod electrode;
 mod error;
 mod heart;
